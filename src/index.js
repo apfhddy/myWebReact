df7dfb29cb8a866ad4d2c228391981bc1d75170f
apfhddy <vavogmlfkr3@gmail.com>
2024-04-03 01:51:15 +0900

    업데이트

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './js/App';
 import reportWebVitals from './config/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   //<React.StrictMode>
-    <App />
+    <App title="d"/>
   //</React.StrictMode>
 );
 
--- src/js/App.js
+++ src/js/App.js
@@ -1,27 +1,11 @@
-import axios from 'axios';
 
-function Test(props){
 
- 
-  return (
-  <div>
-   
-  </div>
-  )
-} 
+
 
 function App() {
-  axios({
-    url:"hi",
-    method:"post",
-    data: {params:"dd"}
-  })
-  .then((res) => {
-    console.log(res)
-  })
   return (
     <div className="App">
-        <Test></Test>
+        hi
     </div>
   );
 }
