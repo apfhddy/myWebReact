7893c54032f2e6abafe19b5127b34b701a18858a
apfhddy <vavogmlfkr3@gmail.com>
2024-04-04 18:56:15 +0900

    업데이트

--- src/js/App.js
+++ src/js/App.js
@@ -7,14 +7,15 @@ import {useState} from 'react';
 
 function App() {
   const [mode,setMode] = useState(0);
- 
+  const [apiKey,setApiKey] = useState(false);
+  const [pop,setPop] = useState('not')
 
   return (
     <div className="App">
         <header>Apfhddy-JAVA-Library</header>
         <main>
-          <div><Side setBody={setMode}/></div>
-          <div><Body mode={mode}/></div>
+          <div><Side setBody={setMode} setApiKey={setApiKey} apiKey={apiKey}/></div>
+          <div><Body mode={mode} apiKey={apiKey}/></div>
         </main>
     </div>
   );
--- src/js/Side.js
+++ src/js/Side.js
@@ -20,6 +20,47 @@ function TopSettings(props){
     for(let i = 0; i < Mode.length; i++){
         list.push(<option value={Mode[i]['real_VER']} key={i}>{Mode[i]['jdk_VER']}</option>);
     }
+    
+
+    let inputCss;
+    let span1;
+    let span2;
+    if(props.apiKey){
+        inputCss = {
+            backgroundColor:"gray"
+        }
+
+        span1 = (
+            <input type='button' value={"인증해제"} onClick={() => {
+                props.setApiKey(false);
+            }}></input>
+        );
+
+        span2 = null;
+    }else{
+        inputCss = {
+            backgroundColor:""
+        }
+
+        span1 = (
+        <input type='button' onClick={(event) => {
+            const key = event.target.form.key.value;
+            axios({
+                method: 'post',
+                url: '/api-certified',
+                data: {key: key}
+            }).then((res) => {
+                if(res){ 
+                    props.setApiKey(res.data);
+                }
+            })
+        }} value={"인증"}/>)
+
+        span2 = (
+            <input type='button' value={"발급"}/>
+        )
+    }
+    
     return (
         <div className='TopSettings'>
             <div>
@@ -31,21 +72,13 @@ function TopSettings(props){
             <div className='Api-Certified'>
                 <form>
                     <span>
-                        <input name="key" className='Api-Input'/>
+                        <input name="key" style={inputCss} readOnly={props.apiKey} className='Api-Input'/>
                     </span>
                     <span>
-                        <input type='button' onClick={(event) => {
-                            const key = event.target.form.key.value;
-                            console.log(key)
-                            axios({
-                                method: 'post',
-                                url: '/api-certified',
-                                data: {key: key}
-                            })
-                        }} value={"인증"}/>
+                        {span1}
                     </span>
                     <span>
-                        <input type='button' value={"발급"}/>
+                        {span2}
                     </span>
                 </form>
             </div>
@@ -79,14 +112,32 @@ function Dirs(props){
         
     },[props.JDK_VER])
 
+    let updateDir;
 
+    if(props.apiKey){
+        updateDir = (
+            <div>
+                <form className='updateDir'>
+                    <span>
+                        Dir
+                    </span>
+                    <span><input type='button' value={"Open"} onClick={()=>{
+                        console.log('hi')
+                    }}/></span>
+                </form>
+            </div>
+        )
+    }
 
     return (
-        <div className='Dirs'>
-            {Mode.map((item,index) => <div key={index}><a href='/'  onClick={(event) => {
-                event.preventDefault();
-                props['setBody'](item['lb_D_no'])
-        }}>{item['name'].substr(1)}</a></div>)}
+        <div  className='Dirs'>
+            {updateDir}    
+            <div>
+                {Mode.map((item,index) => <div key={index}><a href='/'  onClick={(event) => {
+                    event.preventDefault();
+                    props['setBody'](item['lb_D_no'])
+                }}>{item['name'].substr(1)}</a></div>)}
+            </div>
         </div>
     )
 }
@@ -102,8 +153,8 @@ function Side(props){
 
     return (
         <div className='Side'>
-            <TopSettings JDK_VER={default_version} onChangeMode={function(version){setVersion(version);}}/>
-            <Dirs JDK_VER={default_version}  setBody={props['setBody']}/>
+            <TopSettings JDK_VER={default_version} apiKey={props.apiKey} setApiKey={props.setApiKey} onChangeMode={function(version){setVersion(version);}}/>
+            <Dirs JDK_VER={default_version} apiKey={props.apiKey} setBody={props['setBody']}/>
             <Files/>
         </div>
     )
